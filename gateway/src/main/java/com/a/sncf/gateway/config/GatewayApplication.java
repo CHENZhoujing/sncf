4357e67e376e489d39f01989a4c48b1df78b9048
chen_zhoujing <zhoujingchen1@gmail.com>
2025-07-31 18:01:48 +0200

    增加网关模块Gateway

--- gateway/src/main/java/com/a/sncf/gateway/config/GatewayApplication.java
+++ gateway/src/main/java/com/a/sncf/gateway/config/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.a.sncf.member.config;
+package com.a.sncf.gateway.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,13 +9,13 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.a"})
-public class MemberApplication {
+public class GatewayApplication {
 
-    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
+    private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(MemberApplication.class);
+        SpringApplication application = new SpringApplication(GatewayApplication.class);
         Environment env = application.run(args).getEnvironment();
-        LOG.info("Application '{}' is running on port: {}{}", env.getProperty("spring.application.name"), env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
+        LOG.info("Application '{}' is running on port: {}", env.getProperty("spring.application.name"), env.getProperty("server.port"));
     }
 }
