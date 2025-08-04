4584eb1318814bec06ae7cc797ce78a069f4380b
chen_zhoujing <zhoujingchen1@gmail.com>
2025-08-04 17:57:13 +0200

    集成Ant design vue集成图标

--- web/src/main.js
+++ web/src/main.js
@@ -1,4 +1,7 @@
 import './assets/main.css'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
+import * as Icons from '@ant-design/icons-vue';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -10,5 +13,11 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Antd)
 
 app.mount('#app')
+
+const icons = Icons;
+for (const i in icons) {
+  app.component(i, icons[i]);
+}
